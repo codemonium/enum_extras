8025697cb91323a19205c9a3b6ae2c02cfa8840a
Igor Artemenko <igor@codemonium.com>
2021-06-13 20:03:24 -0700

    Initial commit

---
+++ lib/enum_extras.ex
@@ -0,0 +1,18 @@
+defmodule EnumExtras do
+  @moduledoc """
+  Documentation for `EnumExtras`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EnumExtras.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/enum_extras_test.exs
@@ -0,0 +1,8 @@
+defmodule EnumExtrasTest do
+  use ExUnit.Case
+  doctest EnumExtras
+
+  test "greets the world" do
+    assert EnumExtras.hello() == :world
+  end
+end
