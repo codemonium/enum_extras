2924ce39fc281c63540716edc418bbe24e49e936
Igor Artemenko <igor@codemonium.com>
2021-07-04 20:03:27 -0700

    Implement functions for calculating averages

--- lib/enum_extras.ex
+++ lib/enum_extras.ex
@@ -1,18 +1,47 @@
 defmodule EnumExtras do
   @moduledoc """
-  Documentation for `EnumExtras`.
+  Provides additional utility functions for working with enumerables.
   """
+
+  @type t :: Enumerable.t()
 
   @doc """
-  Hello world.
+  Calculates the average of the elements in the `enumerable`.
 
-  ## Examples
+  It should return `nil` if the `enumerable` is empty.
 
-      iex> EnumExtras.hello()
-      :world
-
+  FIXME: Susceptible to floating-point errors.
   """
-  def hello do
-    :world
+  @spec average(t) :: nil | integer
+  def average([]), do: nil
+
+  def average(list) when is_list(list) do
+    Enum.sum(list) / Enum.count(list)
+  end
+
+  @doc """
+  Calculates the weighted average of the elements in the `enumerable`.
+
+  It should return `nil` if the `enumerable` is empty or the weights sum to zero.
+
+  FIXME: Susceptible to floating-point errors.
+
+  TODO: Handle case when number of weights differs from number of elements in list.
+  """
+  @spec weighted_average(t, t) :: nil | integer
+  def weighted_average([], _weights), do: nil
+
+  def weighted_average(list, weights) when is_list(list) and is_list(weights) do
+    case Enum.sum(weights) do
+      0 ->
+        nil
+
+      sum ->
+        total =
+          Enum.zip(list, weights)
+          |> Enum.reduce(0, fn {element, weight}, acc -> acc + element * weight end)
+
+        total / sum
+    end
   end
 end
--- test/enum_extras_test.exs
+++ test/enum_extras_test.exs
@@ -2,7 +2,23 @@ defmodule EnumExtrasTest do
   use ExUnit.Case
   doctest EnumExtras
 
-  test "greets the world" do
-    assert EnumExtras.hello() == :world
+  test "when input is empty, average does not exist" do
+    assert EnumExtras.average([]) == nil
+  end
+
+  test "average" do
+    assert EnumExtras.average([1, 2, 3, 4]) == 2.5
+  end
+
+  test "when input is empty, weighted average does not exist" do
+    assert EnumExtras.weighted_average([], [1, 2, 3]) == nil
+  end
+
+  test "when weights sum to zero, weighted average does not exist" do
+    assert EnumExtras.weighted_average([1, 2, 3], [-1, -2, 3]) == nil
+  end
+
+  test "weighted average" do
+    assert EnumExtras.weighted_average([1, 2, 3, 4], [4, 3, 2, 1]) == 2
   end
 end
